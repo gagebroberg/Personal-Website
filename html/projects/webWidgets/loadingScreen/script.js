c919ded794ba1f1b5a9f95cf60b343c78f94d443
Gage Broberg <gagebroberg@gmail.com>
2022-10-26 21:00:11 -0500

    Adding changes to web widgets and about.

--- html/projects/webWidgets/loadingScreen/script.js
+++ html/projects/webWidgets/loadingScreen/script.js
@@ -1,4 +1,5 @@
 const loadText = document.querySelector('.loading-text')
+const loadTextReplay = document.querySelector('.loading-text-replay')
 const bg = document.querySelector('.bg')
 
 let load = 0
@@ -6,15 +7,30 @@ let load = 0
 let int = setInterval(blurring, 30)
 
 function blurring() {
+
+    console.log(load)
     load++
 
     if (load > 99) {
         clearInterval(int)
     }
 
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    if (load < 99) {
+        loadText.style.display = 'block'
+        loadTextReplay.style.display = 'none'
+        loadText.innerText = `${load}%`
+        loadText.style.opacity = scale(load, 0, 100, 1, 0)
+        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    } else {
+        loadText.style.display = 'none'
+        loadTextReplay.style.display = 'block'
+        loadText.style.opacity = 1
+    }
+}
+
+function replay() {
+    load = 0
+    int = setInterval(blurring, 30)
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
