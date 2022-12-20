e61f566d75dcc1e4a8989a8f36d95c7c068a334a
Gage Broberg <gagebroberg@gmail.com>
2022-12-20 16:57:13 -0600

    Adding basic background swap and responsive design

--- js/index.js
+++ js/index.js
@@ -261,17 +261,114 @@ if (getCookie('visited')) {
 }
 
 // adding a custom menu when right click pressed
-if (document.addEventListener) {
-    document.addEventListener('contextmenu', function(e) {
-    document.getElementById("rmenu").className = "show";
-    document.getElementById("rmenu").style.top = mouseY(event) + 'px';
-    document.getElementById("rmenu").style.left = mouseX(event) + 'px';      
+document.onclick = hideMenu;
+document.oncontextmenu = rightClick;
+const currentApp = document.getElementById("currentApp")
+const appName = document.getElementById("appName")
+    
+function hideMenu() {
+    document.getElementById("contextMenu")
+            .style.display = "none"
+    // document.getElementById("batteryInfo")
+    //         .style.display = "none"
+}
+
+function rightClick(e) {
     e.preventDefault();
-    }, false);
+
+    if (document.getElementById("contextMenu")
+            .style.display == "block")
+        hideMenu();
+    else{
+        var menu = document.getElementById("contextMenu")
+
+        menu.style.display = 'block';
+        menu.style.left = e.pageX + "px";
+        menu.style.top = e.pageY + "px";
+    }
+}
+
+// change desktop background
+function openChangeDesktop() {
+    currentApp.classList.remove("hidden")
+    appName.innerText = "Change Desktop"
+}
+
+// open, close, minimize buttons
+function closeWindow() {
+    currentApp.classList.add("hidden")
+}
+
+function maxWindow() {
+    currentApp.style.width = "100%"
+    currentApp.style.height = "100%"
+}
+
+function minWindow() {
+    currentApp.classList.add("hidden")
+}
+
+
+
+// movable div
+// Make the DIV element draggable:
+dragElement(document.getElementById("currentApp"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
   } else {
-    document.attachEvent('oncontextmenu', function() {
-      alert("You've tried to open context menu");
-      window.event.returnValue = false;
-    });
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
   }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// file reading for image selection
+const reader = new FileReader();
+const fileInput = document.getElementById("file");
+const img = document.getElementById("img");
+reader.onload = e => {
+  img.src = e.target.result;
+}
+fileInput.addEventListener('change', e => {
+  const f = e.target.files[0];
+  reader.readAsDataURL(f);
+})
+
+function setBackgroundImage() {
+    console.log("Attempting to set background")
+    document.body.style.backgroundImage = "url('" + img.src + "')"
+}
 // #endregion
